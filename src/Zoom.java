2d126413936ffe0c6856e7e9911a8faafd77537b
timohaverich <timo.haverich@fh-bielefeld.de>
2022-11-12 22:04:57 +0100

    finished

--- src/Zoom.java
+++ src/Zoom.java
@@ -1,11 +1,9 @@
-import javax.swing.plaf.IconUIResource;
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Zoom {
 
+    // Initialisiert Variable img
     private int[][] img;
 
+    // Methode zum befüllen der Variable img
     public void setImg (int[][] img) {
 
         // prüft, ob leere Arrays übergeben werden
@@ -14,37 +12,59 @@ public class Zoom {
             System.exit(0);
         }
 
-        // prüft, ob Werte innerhalb des Wertebereichs
-        for (int i = 0; i < img.length; i++) {
-            for (int j = 0; j < img[i].length; j++) {
-                if (img[i][j] < 0 || img[i][j] > 7) {
+        for (int[] ints : img) {
+            for (int anInt : ints) {
+
+                // prüft, ob Werte innerhalb des Wertebereichs
+                if (anInt < 0 || anInt > 7) {
                     System.out.println("Array enthält Wert außerhalb des Wertebereichs [0,7].");
                     System.exit(0);
                 }
-            }
-        }
 
-        // prüft, ob Array NxN groß
-        for (int i = 0; i < img.length; i++) {
-            for (int j = 0; j < img[i].length; j++) {
-                if (img.length != img[i].length) {
+                // prüft, ob Array NxN groß
+                if (img.length != ints.length) {
                     System.out.println("Array ist nicht NxN groß.");
                     System.exit(0);
                 }
             }
         }
 
+        // setzt den Wert der Variablen img
         this.img = img;
     }
 
+    // vergrößert das Array um Faktor f
     public int[][] zoom (int f) {
 
+        // erzeugt neues vergrößertes Array mit
         int[][] imgZoom = new int[img.length*f][img.length*f];
 
-        for (int i = 0; i < imgZoom.length; i++) {
+        // row und column zähler
+        int row = 0;
+        int column = 0;
 
+        // prüft, ob f größer als 1 ist
+        if (f <= 1) {
+            System.out.println("f ist nicht größer als 1.");
+            System.exit(0);
+        }
+        else {
+            // weist imgZoom die faktorisierte Anzahl der passenden Werte hinzu
+            for (int[] ints : img) {
+                for (int z = 0; z < f; z++) {
+                    for (int j = 0; j < img[0].length; j++) {
+                        for (int k = 0; k < f; k++) {
+                            imgZoom[row][column] = ints[j];
+                            column++;
+                        }
+                    }
+                    column = 0;
+                    row++;
+                }
+            }
         }
 
+        // gibt das neue Bild zurück
         return imgZoom;
     }
 
--- src/ZoomTest.java
+++ src/ZoomTest.java
@@ -1,18 +1,40 @@
 public class ZoomTest {
     public static void main(String[] args) {
 
-        int[][] testbild = {{1,0},{0,2}};
-        int f = 2;
+        // Testbild 1
+        int[][] testbild1 = {{1,7,0},{1,0,3},{0,2,1}};
+        Zoom zoom1 = new Zoom();
+        zoom1.setImg(testbild1);
 
-        Zoom zoom = new Zoom();
-        zoom.setImg(testbild);
-        ausgabe(zoom.zoom(f));
+        System.out.println("Testbild1 f=2:");
+        ausgabe(zoom1.zoom(2));
+
+        System.out.println();
+
+        System.out.println("Testbild1 f=3:");
+        ausgabe(zoom1.zoom(3));
+
+        System.out.println();
+
+        // Testbild 2
+        int[][] testbild2 = {{1,0,2},{4,2,4},{2,0,1}};
+        Zoom zoom2 = new Zoom();
+        zoom2.setImg(testbild2);
+
+        System.out.println("Testbild2 f=2:");
+        ausgabe(zoom2.zoom(2));
+
+        System.out.println();
+
+        System.out.println("Testbild2 f=3:");
+        ausgabe(zoom2.zoom(3));
     }
 
+    // Methode zum printen der vergrößerten Bilder
     public static void ausgabe(int[][] img) {
-        for ( int i = 0; i < img.length; i++ ) {
-            for ( int j = 0; j < img[i].length; j++ ) {
-                System.out.print( img[i][j] + " ");
+        for (int[] ints : img) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
             }
             System.out.println();
         }
